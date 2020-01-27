bc253bbc3cd8d49a7bebde60e0f1f4de425ffc6a
tony-coder <1606771552@qq.com>
2020-01-27 15:40:08 +0800

    add A1016

--- C++/Advanced Level/1016 Phone Bills (25分).cpp
+++ C++/Advanced Level/1016 Phone Bills (25分).cpp
@@ -1,173 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef struct user
+const int maxn = 1e5;
+int toll[25] = {0};
+struct node
 {
 	string name;
-	string date_on;
-	string date_off;
-
-	user(): name(""), date_on(""), date_off("") {}
-} user;
-
-struct data
-{
-	string name;
-	string date;
-	string status;
-	data(string _name, string _date, string _status)
-	{
-		name = _name;
-		date = _date;
-		status = _status;
-	}
+	int mon, day, hou, min, time, state;
 };
 
-int price[30];
-int pre[30];
-map<string, vector<user> > vis;
-vector<data> record;
-
-bool cmp(data &a, data &b)
+bool cmp(node &a, node &b)
 {
-	return a.name < b.name || (a.name == b.name && a.date < b.date);
+	return a.name != b.name ? a.name < b.name : a.time < b.time;
 }
-
-int change(string s)
+//每个通话记录的相对开销
+//求当前日期与每月1号的相对差值，然后两个日期的相对差值相减就是绝对差值
+double relative_cost(node &call)
 {
-	int ans = 0, k = 1;
-	for (int i = s.size() - 1; i >= 0; --i, k *= 10)
-		ans += (s[i] - '0') * k;
-	return ans;
-}
-
-double get_cost(user &u)
-{
-	int day_on, hour_on, min_on, day_off, hour_off, min_off;
-	day_on = change(u.date_on.substr(3, 2));
-	hour_on = change(u.date_on.substr(6, 2));
-	min_on = change(u.date_on.substr(9, 2));
-
-	day_off = change(u.date_off.substr(3, 2));
-	hour_off = change(u.date_off.substr(6, 2));
-	min_off = change(u.date_off.substr(9, 2));
-
-	double cost = 0;
-	if (day_on == day_off)
-	{
-		if (hour_on == hour_off)
-			cost = price[hour_on + 1] * (min_off - min_on + 1) / 100.;
-		else
-			cost = (price[hour_on + 1] * (60 - min_on) + price[hour_off + 1] * min_off + 60.*(pre[hour_off] - pre[hour_on + 1]) * (hour_off - hour_on - 1)) / 100.;
-	}
-	else
-	{
-		cost = price[hour_on + 1] * (60 - min_on) + price[hour_off + 1] * min_off;
-		cost += ((pre[24] - pre[hour_on + 1]) + (pre[hour_off] - pre[0])) * 60.;
-		cost += ((day_off - day_on - 1) * (pre[24] - pre[0])) * 60;
-		cost /= 100.;
-	}
-	return cost;
-}
-
-int get_time(user &u)
-{
-	int day_on, hour_on, min_on, day_off, hour_off, min_off;
-	day_on = change(u.date_on.substr(3, 2));
-	hour_on = change(u.date_on.substr(6, 2));
-	min_on = change(u.date_on.substr(9, 2));
-
-	day_off = change(u.date_off.substr(3, 2));
-	hour_off = change(u.date_off.substr(6, 2));
-	min_off = change(u.date_off.substr(9, 2));
-
-	double cost = 0;
-	if (day_on == day_off)
-	{
-		if (hour_on == hour_off)
-			cost = (min_off - min_on + 1);
-		else
-			cost =  (60 - min_on) +  min_off + 60.*(hour_off - hour_on - 1);
-	}
-	else
-	{
-		cost = (60 - min_on) + min_off;
-		cost += ((24 - hour_on - 1) + (hour_off)) * 60.;
-		cost += (day_off - day_on - 1) * 60 * 24;
-	}
-	return cost;
+	double total = toll[call.hou] * call.min + call.day * 60 * toll[24];
+	for (int i = 0; i < call.hou; ++i)
+		total += toll[i] * 60;
+	return total / 100.; //
 }
 
 int main(int argc, char const *argv[])
 {
-	memset(pre, 0, sizeof(pre));
-	for (int i = 1; i <= 24; ++i)
+	for (int i = 0; i < 24; ++i)
 	{
-		cin >> price[i];
-		pre[i] = pre[i - 1] + price[i];
+		scanf("%d", &toll[i]);
+		toll[24] += toll[i];
 	}
+	int n;
+	scanf("%d", &n);
+	vector<node>record(n);
 
-	int N;
-	cin >> N;
-	string name, date, status;
-	for (int i = 0; i < N; ++i)
+	for (int i = 0; i < n; ++i)
 	{
-		cin >> name >> date >> status;
-		record.push_back(data(name, date, status));
+		cin >> record[i].name;
+		scanf("%d:%d:%d:%d", &record[i].mon, &record[i].day, &record[i].hou, &record[i].min);
+		string temp;
+		cin >> temp;
+		record[i].state = temp == "on-line" ? 1 : 0;
+		//把时间转换成分钟制
+		record[i].time = record[i].day * 24 * 60 + record[i].hou * 60 + record[i].min;
 	}
 	sort(record.begin(), record.end(), cmp);
 
-	user tmp;
-	for (int i = 0; i < record.size(); ++i)
+	map<string, vector<node> >mp; //存名字与记录向量的对照关系
+	for (int i = 1; i < n; ++i)
 	{
-		if (tmp.name == record[i].name)
+		if (record[i].name == record[i - 1].name && record[i - 1].state == 1 && record[i].state == 0)
 		{
-			if (record[i].status == "on-line" && tmp.date_off == "")
-			{
-				tmp.date_on = record[i].date;
-			}
-			else if (record[i].status == "on-line" && tmp.date_off != "")
-			{
-				tmp.date_on = record[i].date;
-				tmp.date_off = "";
-			}
-			else if (record[i].status == "off-line" && tmp.date_on != "")
-			{
-				tmp.date_off = record[i].date;
-				vis[tmp.name].push_back(tmp);
-				tmp.name = "";
-				tmp.date_on = "";
-				tmp.date_off = "";
-			}
-			else if (record[i].status == "off-line" && tmp.date_on == "")
-			{
-				tmp.date_off = record[i].date;
-			}
+			mp[record[i - 1].name].push_back(record[i - 1]);
+			mp[record[i].name].push_back(record[i]);
 		}
-		else
-		{
-			tmp.name = record[i].name;
-			if (record[i].status == "on-line")
-				tmp.date_on = record[i].date;
-			else
-				tmp.date_off = record[i].date;
-			continue;
-		}
-
 	}
-	map<string, vector<user> >::iterator iter = vis.begin();
-	for (; iter != vis.end(); iter++)
+	map<string, vector<node> >::iterator iter;
+	for (iter = mp.begin(); iter != mp.end(); iter++)
 	{
-		cout << iter->first << " " << (iter->second)[0].date_on.substr(0, 2) << endl;
-		double tot = 0.;
-		for (int i = 0; i < (iter->second).size(); ++i)
+		vector<node>tmp = iter->second;
+		cout << iter->first;
+		printf(" %02d\n", tmp[0].mon);
+		double total = 0;
+		for (int i = 0; i < tmp.size(); i += 2)
 		{
-			// cout <<  (iter->second)[i].date_on << " " <<  (iter->second)[i].date_off << endl;
-			double cost = get_cost((iter->second)[i]);
-			printf("%s %s %d $%.2f\n", (iter->second)[i].date_on.substr(3, 8).c_str(), (iter->second)[i].date_off.substr(3, 8).c_str(), get_time((iter->second)[i]), cost);
-			tot += cost;
+			double t = relative_cost(tmp[i + 1]) - relative_cost(tmp[i]);
+			printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", tmp[i].day, tmp[i].hou, tmp[i].min, tmp[i + 1].day, tmp[i + 1].hou, tmp[i + 1].min, tmp[i + 1].time - tmp[i].time, t );
+			total += t;
 		}
-		printf("Total amount: $%.2f\n", tot );
+		printf("Total amount: $%.2f\n", total);
 	}
 
 	return 0;
