aa41c095a282e27487596fb1f9db09eacdacf673
tony-coder <1606771552@qq.com>
2020-01-29 15:06:34 +0800

    add A1036(优化)

--- C++/Advanced Level/1036 Boys vs Girls (25分).cpp
+++ C++/Advanced Level/1036 Boys vs Girls (25分).cpp
@@ -1,63 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 0x3f3f3f3f;
-struct node
-{
-	// char name[10];
-	// char id[10];
+struct node {
 	string name, id;
-	char gender;
 	int grade;
-	node(): name(""), id(""), gender(0), grade(0) {}
-	node(string _name, char _gender, string _id, int _grade): name(_name), gender(_gender), id(_id), grade(_grade) {}
-	void operator=(node & n)
-	{
-		name = n.name;
-		id = n.id;
-		gender = n.gender;
-		grade = n.grade;
-	}
-};
+} male, female;
 
 int main(int argc, char const *argv[])
 {
 	int N;
 	cin >> N;
-	// vector<node> stu(N);
-	node male("", 'M', "", INF);
-	node female("", 'F', "", 0);
-	for (int i = 0; i < N; ++i)
-	{
-		node tmp;
-		// scanf("%s %c %s %d",stu[i].name,&stu[i].gender,stu[i].id,&stu[i].grade);
-		cin >> tmp.name >> tmp.gender >> tmp.id >> tmp.grade;
-		if (tmp.gender == 'M' && tmp.grade < male.grade)
-			male = tmp;
-		else if (tmp.gender == 'F' && tmp.grade > female.grade)
-			female = tmp;
+	string name, id;
+	char gender;
+	int grade;
+	int max_f = -1, min_m = INF;
+	for (int i = 0; i < N; ++i) {
+		cin >> name >> gender >> id >> grade;
+		if (gender == 'M' && grade < min_m) {
+			male = {name, id, grade};
+			min_m = grade;
+		} else if (gender == 'F' && grade > max_f) {
+			female = {name, id, grade};
+			max_f = grade;
+		}
 	}
-	bool flag = false;
-
-	if (female.id != "")
+	if (max_f != -1)
 		printf("%s %s\n", female.name.c_str(), female.id.c_str());
 	else
-	{
-		flag = true;
 		printf("Absent\n");
-	}
-
-	if (male.id != "")
+	if (min_m != INF)
 		printf("%s %s\n", male.name.c_str(), male.id.c_str());
 	else
-	{
-		flag = true;
 		printf("Absent\n");
-	}
-
-	if (flag)
+	if (min_m == INF || max_f == -1)
 		printf("NA\n");
 	else
 		printf("%d\n", female.grade - male.grade);
-
 	return 0;
 }
