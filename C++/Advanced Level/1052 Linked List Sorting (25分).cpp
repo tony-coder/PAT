eaa791a99af9cee7e323455fb2ba10030f7f09b3
tony-coder <1606771552@qq.com>
2020-02-01 22:41:39 +0800

    add A1052

--- C++/Advanced Level/1052 Linked List Sorting (25分).cpp
+++ C++/Advanced Level/1052 Linked List Sorting (25分).cpp
@@ -1,101 +1,47 @@
-//有一个测试点超时
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 1e6 + 10;
 
 struct node
 {
-	int Address;
-	int Key, Next, Prev;
-} List[maxn];
+	int Address, Key, Next;
+} l[maxn], ans[maxn];
 
-int N, head;
-
-void Swap(node& n1, node& n2) {
-	node tmp = {n2.Address, n2.Key, n2.Next, n2.Prev};
-	if (n1.Next == n2.Address) //相邻
-	{
-		List[n2.Address].Next = n1.Address;
-		List[n2.Address].Prev = n1.Prev;
-		if (n1.Prev == 0)
-			head = n2.Address;
-		else
-			List[n1.Prev].Next = n2.Address;
-		List[n1.Prev].Next = n2.Address;
-
-		List[n1.Address].Next = tmp.Next;
-		List[n1.Address].Prev = n2.Address;
-
-		if (tmp.Prev == 0)
-			head = n1.Address;
-	}
-	else
-	{
-		List[n2.Address].Next = n1.Next;
-		List[n2.Address].Prev = n1.Prev;
-
-		if (n1.Prev == 0)
-			head = n2.Address;
-		else
-			List[n1.Prev].Next = n2.Address;
-
-		List[n1.Prev].Next = n2.Address;
-
-		List[n1.Address].Next = tmp.Next;
-		List[n1.Address].Prev = tmp.Prev;
-
-		if (tmp.Prev == 0)
-			head = n1.Address;
-		else
-			List[tmp.Prev].Next = n1.Address;
-	}
-
-	List[List[n2.Address].Next].Prev = n2.Address;
-	List[List[n1.Address].Next].Prev = n1.Address;
-}
-
-int sort() {
-	int cnt = 0;
-	for (int i = head; i != -1; i = List[i].Next) {
-		cnt++;
-		for (int j = List[i].Next; j != -1; j = List[j].Next) {
-			if (List[i].Key > List[j].Key) {
-				Swap(List[i], List[j]);
-				swap(i, j);
-			}
-		}
-	}
-	return cnt;
+int cmp(node& a, node& b) {
+	return a.Key < b.Key;
 }
 
 int main(int argc, char const *argv[])
 {
-	scanf("%d%d", &N, &head);
-	for (int i = 0; i < N; ++i)
-	{
+	ios::sync_with_stdio(false);
+	int N, head;
+	cin >> N >> head;
+	for (int i = 1; i <= N; ++i) {
 		int address, key, next;
-		scanf("%d%d%d", &address, &key, &next);
-		List[address].Address = address;
-		List[address].Key = key;
-		List[address].Next = next;
-		List[next].Prev = address;
+		cin >> address >> key >> next;
+		l[address].Address = address;
+		l[address].Key = key;
+		l[address].Next = next;
 	}
-	int cnt = sort();
+	int cnt = 0;
+	while (head != -1) {
+		cnt++;
+		ans[cnt].Address = l[head].Address;
+		ans[cnt].Key = l[head].Key;
+		head = l[head].Next;
+	}
 	if (cnt == 0) {
-		printf("0 -1\n");
+		cout << "0 -1" << endl; //特判 最后一个点
 		return 0;
 	}
-	printf("%d %5d\n", cnt, head);
-	for (int i = head; i != -1; i = List[i].Next)
-	{
-		if (List[i].Next == -1) {
-			printf("%05d %d -1\n", List[i].Address, List[i].Key);
-			break;
+	sort(ans + 1, ans + 1 + cnt, cmp);
+	printf("%d %05d\n", cnt, ans[1].Address);
+	for (int i = 1; i <= cnt; ++i) {
+		if (i != cnt) {
+			printf("%05d %d %05d\n", ans[i].Address, ans[i].Key, ans[i + 1].Address);
+		} else {
+			printf("%05d %d -1\n", ans[i].Address, ans[i].Key );
 		}
-		else
-			printf("%05d %d %05d\n", List[i].Address, List[i].Key, List[i].Next);
 	}
-
-
 	return 0;
 }
