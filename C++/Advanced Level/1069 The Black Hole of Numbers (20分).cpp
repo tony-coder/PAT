a0ce274c10db4a88062cddaca498fda3b07a0adc
tony-coder <1606771552@qq.com>
2020-02-02 17:09:35 +0800

    add A1069

--- C++/Advanced Level/1069 The Black Hole of Numbers (20分).cpp
+++ C++/Advanced Level/1069 The Black Hole of Numbers (20分).cpp
@@ -1,32 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char s[6];
 bool cmp(char a, char b) {
 	return a > b;
 }
 
 int main(int argc, char const *argv[])
 {
-	scanf("%s", s);
+	string s;
+	cin >> s;
 
 	while (true) {
-		while (strlen(s) < 4) //补全
-			s[strlen(s)] = '0';
-		s[4] = '\0';
+		while (s.size() < 4) //补全
+			s.push_back('0');
 		int Max, Min;
-		sort(s, s + 4);
-		// Min = atoi(s);
-		sscanf(s, "%d", &Min);
-		sort(s, s + 4, cmp);
-		// Max = atoi(s);
-		sscanf(s, "%d", &Max);
-
+		sort(s.begin(), s.end());
+		Min = stoi(s);
+		sort(s.begin(), s.end(), cmp);
+		Max = stoi(s);
 		printf("%04d - %04d = %04d\n", Max, Min, Max - Min );
 
 		if (Max - Min == 6174 || Max - Min == 0)
 			break;
-		sprintf(s, "%d", Max - Min);
+		s = to_string(Max - Min);
 	}
 
 	return 0;
