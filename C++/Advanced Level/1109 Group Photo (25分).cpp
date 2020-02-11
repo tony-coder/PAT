aee5f9558957cc5dafb26b5e42f822d50425e9d2
tony-coder <1606771552@qq.com>
2020-02-11 17:02:49 +0800

    add A1109

--- C++/Advanced Level/1109 Group Photo (25分).cpp
+++ C++/Advanced Level/1109 Group Photo (25分).cpp
@@ -1,59 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node {
-	string name;
-	int height;
-};
-
 int main(int argc, char const *argv[])
 {
 	ios::sync_with_stdio(false);
 	int N, K;
 	cin >> N >> K;
-	vector<node> v(N);
-	vector<string> ans(N, "");
+	vector<pair<string, int> > a(N);
 	for (int i = 0; i < N; ++i)
-		cin >> v[i].name >> v[i].height;
-	sort(v.begin(), v.end(), [](node & a, node & b) {return a.height == b.height ? a.name > b.name : a.height < b.height;});
-	int row = K, now;
-	K = N / row;
-	for (now = 0; now < (row - 1)*K; now += K) {
-		int i = now + K / 2 - 1, j = now + K / 2 + 1;
-		int pos = now + K - 1;
-		ans[now + K / 2] = v[pos--].name;
-		for (int k = 0; k < K - 1; ++k) {
-			if (k % 2 == 0) {
-				ans[i--] = v[pos--].name;
-			} else {
-				ans[j++] = v[pos--].name;
-			}
+		cin >> a[i].first >> a[i].second;
+	sort(a.begin(), a.end(), [](pair<string, int> &a, pair<string, int> &b)->bool{
+		if (a.second == b.second)
+			return a.first < b.first;
+		return a.second > b.second;
+	});
+	int s = 0;  //每排输出时保存的开始位置
+	int f;  	//结束位置
+	for (int i = 0; i < K; ++i) {
+		f = (0 == i ? N % K : 0) + N / K + s - 1;
+		for (int j = f - 1 + ((f - s) % 2); j >= s; j -= 2)
+			cout << a[j].first << " ";
+		for (int j = s; j <= f; j += 2) {
+			if (j != s)
+				cout << " ";
+			cout << a[j].first;
 		}
-	}
-	int col = N - (row - 1) * K;
-	int i = now + col / 2 - 1, j = now + col / 2 + 1;
-	int pos = now + col - 1;
-	ans[now + col / 2] = v[pos--].name;
-	for (int k = 0; k < col - 1; ++k) {
-		if (k % 2 == 0) {
-			ans[i--] = v[pos--].name;
-		} else {
-			ans[j++] = v[pos--].name;
-		}
-	}
-	for (j = N - col; j < N - 1; ++j)
-		cout << ans[j] << " ";
-	cout << ans[j] << endl;
-	for (i = row - 2; i >= 0; --i) {
-		for (j = i * K; j < (i + 1)*K - 1; ++j)
-			cout << ans[j] << " ";
-		cout << ans[j] << endl;
+		cout << endl;
+		s = f + 1;
 	}
 
 	return 0;
 }
 /*
-10 2
+10 4
 Tom 188
 Mike 170
 Eva 168
