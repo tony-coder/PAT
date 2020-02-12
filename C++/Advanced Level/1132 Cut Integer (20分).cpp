e84f03ece7d91c909b573536617a6294a3fc0def
tony-coder <1606771552@qq.com>
2020-02-12 15:45:28 +0800

    add A1132

--- C++/Advanced Level/1132 Cut Integer (20分).cpp
+++ C++/Advanced Level/1132 Cut Integer (20分).cpp
@@ -7,27 +7,16 @@ int main(int argc, char const *argv[])
 	int N;
 	cin >> N;
 	LL Z;
-	stringstream ss;
 	while (N--) {
 		cin >> Z;
-		string z_s;
-		ss << Z;
-		ss >> z_s;
-		ss.clear();
+		string z_s = to_string(Z);
 		int len = z_s.length() / 2;
 		string s1, s2;
 		s1 = z_s.substr(0, len);
 		s2 = z_s.substr(len);
-		// cout << s1 << " " << s2 << endl;
 		LL t1, t2;
-		ss << s1;
-		ss >> t1;
-		ss.clear();
-		ss << s2;
-		ss >> t2;
-		ss.clear();
-		// t1 = stoi(s1);
-		// t2 = stoi(s2);
+		t1 = stoll(s1);
+		t2 = stoll(s2);
 		if (t1 == 0 || t2 == 0)
 			cout << "No" << endl;
 		else if (Z % (t1 * t2) == 0)
