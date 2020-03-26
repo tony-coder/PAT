0a95031dd89c9726e5465b7c320dde6ade4a0ed3
tony-coder <1606771552@qq.com>
2020-03-26 19:12:40 +0800

    add A1143

--- C++/Advanced Level/1143 Lowest Common Ancestor (30分).cpp
+++ C++/Advanced Level/1143 Lowest Common Ancestor (30分).cpp
@@ -1,70 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node {
-	int val;
-	node* left;
-	node* right;
-};
-
-map<int, node*>mp;
-
-node* insert(node* root, int v) {
-	if (root == NULL) {
-		root = new node;
-		root->val = v;
-		root->left = NULL;
-		root->right = NULL;
-		mp[root->val] = root;
-		return root;
-	}
-	if (v > root->val)
-		root->right = insert(root->right, v);
-	else
-		root->left = insert(root->left, v);
-	return root;
-}
-
-node* LCA(node* root, node* a, node* b) {
-	if (root == NULL || root == a || root == b)
-		return root;
-	node* left = LCA(root->left, a, b);
-	node* right = LCA(root->right, a, b);
-	if (left && right)
-		return root;
-	if (left)
-		return left;
-	if (right)
-		return right;
-	return NULL;
-}
-
 int main(int argc, char const *argv[])
 {
-	int M, N, t;
+	int M, N;
 	cin >> M >> N;
-	node* root = NULL;
+	vector<int>pre(N);
+	map<int, int>mp;
 	for (int i = 0; i < N; ++i) {
-		cin >> t;
-		root = insert(root, t);
+		cin >> pre[i];
+		mp[pre[i]]++;
 	}
 	for (int i = 0; i < M; ++i) {
-		int a, b;
+		int a, b, ans;
 		cin >> a >> b;
 		if (!mp.count(a) && !mp.count(b))
 			printf("ERROR: %d and %d are not found.\n", a, b );
 		else if (!mp.count(a))
 			printf("ERROR: %d is not found.\n", a);
 		else if (!mp.count(b))
-			printf("ERROR: %d is not found.\n", b);
+			printf("ERROR: %d is not found.\n", b );
 		else {
-			node* ans = LCA(root, mp[a], mp[b]);
-			if (ans->val == a)
-				printf("%d is an ancestor of %d.\n", a, b );
-			else if (ans->val == b)
+			for (int j = 0; j < N; ++j) {
+				ans = pre[j];
+				if ((ans < a && ans > b) || (ans > a && ans < b) || ans == a || ans == b)
+					break;
+			}
+			if (ans == a)
+				printf("%d is an ancestor of %d.\n", a, b);
+			else if (ans == b)
 				printf("%d is an ancestor of %d.\n", b, a );
 			else
-				printf("LCA of %d and %d is %d.\n", a, b, ans->val);
+				printf("LCA of %d and %d is %d.\n", a, b, ans );
 		}
 	}
 
