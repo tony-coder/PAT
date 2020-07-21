3f5dc7df3dd5a0912993e49c6dc22f9ea6b11fce
tony-coder <1606771552@qq.com>
2020-07-21 23:32:52 +0800

    Update 1071 Speech Patterns (25分).cpp

--- C++/Advanced Level/1071 Speech Patterns (25分).cpp
+++ C++/Advanced Level/1071 Speech Patterns (25分).cpp
@@ -3,32 +3,28 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	string str, tmp;
-	getline(cin, str);
-	map<string, int> mp;
-	for (int i = 0; i < str.length(); ++i) {
-		if (isalnum(str[i])) {
-			tmp += tolower(str[i]);
-		} else if (!isalnum(str[i]) && i != str.length() - 1) {
-			if (tmp.length())
-				mp[tmp]++;
-			tmp = "";
-		}
-		if (i == str.length() - 1) {
+	string s, tmp;
+	map<string, int>mp;
+	getline(cin, s);
+	for (size_t i = 0; i < s.length(); ++i) {
+		if (isalnum(s[i])) {
+			tmp += tolower(s[i]);
+		} else if (!isalnum(s[i])) {
 			if (tmp.length())
 				mp[tmp]++;
 			tmp = "";
 		}
 	}
+	if (tmp.length())
+		mp[tmp]++;
 	int Max = 0;
 	string ans;
-	for (auto& t : mp) {
+	for (auto t : mp) {
 		if (t.second > Max) {
 			Max = t.second;
 			ans = t.first;
 		}
 	}
-	cout << ans << " " << mp[ans] << endl;
-
+	printf("%s %d\n", ans.c_str(), Max);
 	return 0;
 }
